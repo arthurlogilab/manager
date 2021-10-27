918c25790ec86e721fb274abe0a92edc3e7b187d
JeremyDec <jeremy.de-cesare@ovhcloud.com>
2021-10-27 17:12:33 +0200

    feat(netapp): add snapshot policies feature
    ref: MANAGER-7153
    
    Signed-off-by: JeremyDec <jeremy.de-cesare@ovhcloud.com>

---
+++ packages/manager/modules/netapp/src/dashboard/snapshot-policies/create/constants.js
@@ -0,0 +1,8 @@
+export const MONTHS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+export const WEEKDAYS = [1, 2, 3, 4, 5, 6, 7];
+
+export default {
+  MONTHS,
+  WEEKDAYS,
+};
--- packages/manager/modules/netapp/src/dashboard/snapshot-policies/routing.js
+++ packages/manager/modules/netapp/src/dashboard/snapshot-policies/routing.js
@@ -1,8 +1,34 @@
 export default /* @ngInject */ ($stateProvider) => {
   $stateProvider.state('netapp.dashboard.snapshotPolicies', {
     url: '/snapshotPolicies',
-    component: 'ovhManagerNetAppSnapshotPolicies',
+    views: {
+      '@netapp.dashboard': {
+        component: 'ovhManagerNetAppSnapshotPolicies',
+      },
+    },
+
     resolve: {
+      goToCreateSnapshotPolicies: /* @ngInject */ ($state) => () =>
+        $state.go('netapp.dashboard.snapshotPolicies.create'),
+      getSnapshotPolicies: /* @ngInject */ ($http, $q, serviceName) => () =>
+        $http
+          .get(`/storage/netapp/${serviceName}/snapshotPolicy`)
+          .then(({ data: snapshotPolicyIds }) =>
+            $q
+              .all(
+                snapshotPolicyIds.map(({ id }) =>
+                  $http
+                    .get(`/storage/netapp/${serviceName}/snapshotPolicy/${id}`)
+                    .then(({ data: snapshotPolicy }) => snapshotPolicy),
+                ),
+              )
+              .then((snapshotPolicies) =>
+                snapshotPolicies.filter(({ status }) => status !== 'deleting'),
+              ),
+          ),
+      snapshotPolicies: /* @ngInject */ (getSnapshotPolicies) =>
+        getSnapshotPolicies(),
+
       breadcrumb: /* @ngInject */ ($translate) =>
         $translate.instant('netapp_snapshot_policies_breadcrumb'),
     },
