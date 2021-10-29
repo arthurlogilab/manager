ef0094c3c1840b8ff1007f7f102c532365e41271
Maël LE GAL <mael.le-gal@corp.ovh.com>
2021-10-29 11:04:58 +0200

    feat(ai-training-jobs): add possibility to attach ssh keys
    Signed-off-by: Maël LE GAL <mael.le-gal@corp.ovh.com>

--- packages/manager/modules/pci/src/projects/project/training/training.constants.js
+++ packages/manager/modules/pci/src/projects/project/training/training.constants.js
@@ -1,14 +1,19 @@
 export const GUIDE_URL = 'https://docs.ovh.com/gb/en/ai-training';
 export const COMMUNITY_URL = 'https://community.ovh.com/en/c/Data-AI';
 export const DISCORD_URL = 'https://discord.com/invite/vXVurFfwe9';
-export const JOB_MAX_SSH_KEYS = 10;
 export const DOC_DOCKER_BUILD_URL =
   'https://docs.ovh.com/gb/en/ai-training/build-use-custom-image/';
+export const JOB_SSH_KEYS_CONSTANTS = {
+  MAX: 10,
+  CUSTOM_SELECT: '-',
+  PLACEHOLDER: 'ssh-rsa AAAAB3...',
+  NB_ROWS: 5,
+};
 
 export default {
   GUIDE_URL,
   COMMUNITY_URL,
   DISCORD_URL,
   DOC_DOCKER_BUILD_URL,
-  JOB_MAX_SSH_KEYS,
+  JOB_SSH_KEYS_CONSTANTS,
 };
